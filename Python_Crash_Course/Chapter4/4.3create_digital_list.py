d3f23ee3a8ecbedf3fd8277021027d66b8a4af19
huqiaonan01 <*2008*hu>
2020-05-08 17:04:20 +0800

    4.3节的答案

---
+++ Python_Crash_Course/Chapter4/4.3create_digital_list.py
@@ -0,0 +1,43 @@
+# #4-3
+# for i in range(1,21):
+#     print(i)
+# #4-4
+# list_big = list(range(1,1000001))
+# for num in list_big:
+#     print(num)
+#
+# #4-5
+# list_big2 = list(range(1,1000001))
+# print(min(list_big2))
+# print(max(list_big2))
+# print(sum(list_big2))
+
+# #4-6
+# list_odd = list(range(1,20,2))
+# for num in list_odd:
+#     print(num)
+# print(list_odd)
+
+# #4-7
+# list_3multiple = [value for value in range(3,31) if value%3==0]
+# for num in list_3multiple:
+#     print(num)
+    
+# list_3multiple2 = []
+# for i in range(3,31,3):
+#     list_3multiple2.append(i)
+# print(list_3multiple2)
+# for num in list_3multiple2:
+#     print(num)
+    
+# #4-8
+list_cube = []
+for i in range(1,11):
+    list_cube.append(i**3)
+for num in list_cube:
+    print(num)
+
+# #4-9
+# list_cube2 = [value**3 for value in range(1,11)]
+# for num in list_cube2:
+#     print(num)
